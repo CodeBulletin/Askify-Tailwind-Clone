87f9a10b136411ce14138923d506873bc9c2f824
CodeBulletin <CodeBulletin@github.com>
2022-07-26 15:18:32 +0530

    + Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./*.{html,js}"],
     theme: {
-      extend: {},
+        screens: {
+            xs: '100px',
+            sm: '400px',
+            md: '768px',
+            lg: '1080px',
+            xl: '1440px',
+            xxl: '2160px'
+        },
+        extend: {
+            colors: {
+                askifyBGYello: '#f2f0e3',
+                askifyGrayishYello: 'hsl(55, 22%, 89%)',
+                askifyBrightYello: 'hsl(62, 55%, 87%)',
+                askifyWhite: 'hsl(55, 52%, 96%)',
+                textPurple: 'hsl(300, 46%, 34%)'
+            }
+        },
     },
     plugins: [],
   }
